366f4543986666286095cbf5850513979274c487
Jacob Santos <github@jacobsantos.com>
2018-08-13 02:10:17 -0500

    Coding Challenge: Problem 2 - Complete functionality. Needs testing.

--- problem2/app/Player.php
+++ problem2/app/Player.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
-    //
+    protected $table = 'player';
+
+    protected $fillable = [
+    	'name',
+		'player_id',
+		'credits',
+		'lifetime_spins',
+	];
+
+    protected $guarded = [
+    	'salt_value',
+	];
+
+    public $timestamps = false;
+
+    public function getLifetimeAverageAttribute()
+	{
+		if ($this->lifetime_spins < 1) {
+			return 0;
+		}
+
+		return round($this->credits / $this->lifetime_spins, 2);
+	}
 }
--- problem2/routes/web.php
+++ problem2/routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/spin', \App\Http\Controllers\SpinController::class . '@store');
